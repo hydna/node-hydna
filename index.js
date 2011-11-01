cda605966c7d4c163a96848a228397971f8e1225
Johan Dahlberg <dahlberg.johan@gmail.com>
2011-11-01 21:06:15 +0100

    new date

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //
-//        Copyright 2010 Hydna AB. All rights reserved.
+//        Copyright 2011 Hydna AB. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions
