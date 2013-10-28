c575184167f395d0e5c153847cf74cb2e7dd701e
Johan Dahlberg <dahlberg.johan@gmail.com>
2013-10-28 20:56:42 +0100

    Added tis push test

---
+++ test/test-push-tls.js
@@ -0,0 +1,47 @@
+var assert              = require('assert');
+var hydna               = require('../index');
+
+var common              = require('./common');
+
+var host;
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; 
+
+common.timeout(5000);
+
+process.nextTick(testSend);
+
+host = 'https://' + common.TEST_HOST;
+
+function testSend() {
+  hydna.send(host, 'Testdata', function(err) {
+    assert.ifError(err);
+    testSendDeny();
+  });
+}
+
+
+function testSendDeny() {
+  hydna.send(host + '/open-deny', 'Testdata', function(err) {
+    assert.ok(err instanceof hydna.OpenError);
+    assert.equal(err.message, 'DENIED');
+    testEmit();
+  });  
+}
+
+
+function testEmit() {
+  hydna.dispatch(host, 'Testdata', function(err) {
+    assert.ifError(err);
+    testEmitDeny();
+  });  
+}
+
+
+function testEmitDeny() {
+  hydna.dispatch(host + '/open-deny', 'Testdata', function(err) {
+    assert.ok(err instanceof hydna.OpenError);
+    assert.equal(err.message, 'DENIED');
+    common.shutdown();
+  });  
+}
